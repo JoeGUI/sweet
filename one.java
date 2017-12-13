a837dc53548c54a52bf6fc325f79843e57a28067
JoeGUI <33052853+JoeGUI@users.noreply.github.com>
2017-12-13 14:38:49 +0800

    Update one.java

--- one.java
+++ one.java
@@ -1,3 +1,6 @@
 String[] stringArray = { "a", "b", "c", "d", "e" };  
 ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(stringArray));  
 String[] stringArr = new String[arrayList.size()];  
+arrayList.toArray(stringArr);
+for (String s : stringArr)
+  System.out.println(s);
