d1fd8f7b831244e527fae668dc624059f4cc6ce2
BigSweetsweet <33142228+BigSweetsweet@users.noreply.github.com>
2017-12-13 14:23:06 +0800

    Create one.java

---
+++ one.java
@@ -0,0 +1,3 @@
+String[] stringArray = { "a", "b", "c", "d", "e" };  
+ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(stringArray));  
+String[] stringArr = new String[arrayList.size()];  
